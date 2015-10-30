b1a4ce8734da98e63596b56740aa426e55b4cbc1
songbo <songb@bsfit.com.cn>
2015-10-30 17:26:10 +0800

    最新更新

--- MyFirstGitPro/src/com/boge/first/Main.java
+++ MyFirstGitPro/src/com/boge/first/Main.java
@@ -4,5 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("这是我的啊，亲！！hahahahaha");
+		
+		
+		
+		System.out.println("woshiasshiashaishai");
 	}
 }
